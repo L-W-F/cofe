0082d5eb1e987b094067e91cd942d211de1709cb
李文富 <liwenfu@crossjs.com>
2021-09-12 15:38:15 +0800

    feature(favicon): add svg as favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
         <meta charSet="utf-8" />
         <title>COFE</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" href="favicon.svg" />
       </Head>
       <Store initialStates={pageProps.initialStates}>
         <Theme>
