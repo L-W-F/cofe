fd34eab5eada1a7c51e4d3961cc124582dd11630
crossjs <liwenfu@crossjs.com>
2021-09-24 13:55:26 +0800

    refactor(header): 移动对应的子组件到 Header 文件内

--- applications/studio/src/components/Header.tsx
+++ applications/studio/src/components/Header.tsx
@@ -1,17 +1,78 @@
 import React from 'react';
 import NextLink from 'next/link';
+import { useRouter } from 'next/router';
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
 import {
+  Avatar,
   Box,
   Flex,
   FlexProps,
   Icon,
+  IconButton,
   Link,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  useColorMode,
   useColorModeValue,
 } from '@chakra-ui/react';
-import { GithubIcon } from '@cofe/icons';
-import { ColorModeSwitch } from './header/ColorModeSwitch';
-import { Logo } from './header/Logo';
-import { Whoami } from './header/Whoami';
+import { CofeIcon, GithubIcon } from '@cofe/icons';
+import { useDispatch, useStore } from '@cofe/store';
+import { CofeWhoami } from '@cofe/types';
+import { post } from 'utils/io';
+
+const Logo = () => {
+  return <Icon as={CofeIcon} w="5" h="5" />;
+};
+
+const ColorModeSwitch = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  return (
+    <IconButton
+      aria-label="Toggle color mode"
+      variant="ghost"
+      icon={colorMode === 'dark' ? <SunIcon /> : <MoonIcon />}
+      onClick={toggleColorMode}
+    />
+  );
+};
+
+const Whoami = () => {
+  const whoami = useStore<CofeWhoami>('whoami');
+  const dispatch = useDispatch();
+  const { push } = useRouter();
+
+  if (!whoami?.username) {
+    return null;
+  }
+
+  return (
+    <Menu matchWidth size="small">
+      <MenuButton aria-label={whoami.username}>
+        <Avatar size="xs" name={whoami.username} />
+      </MenuButton>
+      <MenuList minW="initial">
+        <NextLink href="/account" passHref>
+          <MenuItem as={Link}>账户</MenuItem>
+        </NextLink>
+        <MenuItem
+          onClick={async (e) => {
+            try {
+              await post('/api/logout', null);
+              dispatch('CLEAR_LOGIN')(null);
+
+              push('/login');
+            } catch (error) {}
+          }}
+        >
+          退出
+        </MenuItem>
+      </MenuList>
+    </Menu>
+  );
+};
 
 export const Header = ({ children = <Box flex={1} /> }: FlexProps) => {
   return (
--- applications/studio/src/components/header/Logo.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Icon } from '@chakra-ui/react';
-import { CofeIcon } from '@cofe/icons';
-
-export const Logo = () => {
-  return <Icon as={CofeIcon} w="5" h="5" />;
-};
