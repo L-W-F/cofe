1fdf08cab6854a4ed73fd9a1cbbe02afcb30ca31
crossjs <liwenfu@crossjs.com>
2021-09-26 01:50:24 +0800

    fix(logger): 暂时禁用文件保存功能

--- packages/logger/logger.ts
+++ packages/logger/logger.ts
@@ -1,72 +1,72 @@
-import * as fs from 'fs';
-import * as path from 'path';
-import * as util from 'util';
+// import * as fs from 'fs';
+// import * as path from 'path';
+// import * as util from 'util';
 import _debug from 'debug';
 
 const logger = _debug('c');
 
 const map = new Map<string, _debug.Debugger>();
 
-let logFilename: string;
-let writeStream: fs.WriteStream;
+// let logFilename: string;
+// let writeStream: fs.WriteStream;
 
-const writeToFile = (
-  time: string,
-  service: string,
-  level: string,
-  message: string,
-) => {
-  try {
-    const folder = path.join(process.cwd(), process.env.LOGGER_ROOT);
-    const filename = time.slice(0, 10);
+// const writeToFile = (
+//   time: string,
+//   service: string,
+//   level: string,
+//   message: string,
+// ) => {
+//   try {
+//     const folder = path.join(process.cwd(), process.env.LOGGER_ROOT);
+//     const filename = time.slice(0, 10);
 
-    if (logFilename !== filename) {
-      logFilename = filename;
+//     if (logFilename !== filename) {
+//       logFilename = filename;
 
-      /* istanbul ignore if */
-      if (writeStream) {
-        writeStream.end();
-        writeStream = null;
-      }
-    }
+//       /* istanbul ignore if */
+//       if (writeStream) {
+//         writeStream.end();
+//         writeStream = null;
+//       }
+//     }
 
-    const filepath = path.join(folder, logFilename);
+//     const filepath = path.join(folder, logFilename);
 
-    const cb = () => {
-      if (!writeStream) {
-        writeStream = fs.createWriteStream(filepath, { flags: 'a' });
-      }
+//     const cb = () => {
+//       if (!writeStream) {
+//         writeStream = fs.createWriteStream(filepath, { flags: 'a' });
+//       }
 
-      writeStream.write(
-        `${JSON.stringify({
-          time,
-          service,
-          level,
-          message,
-        })}\n`,
-      );
-    };
+//       writeStream.write(
+//         `${JSON.stringify({
+//           time,
+//           service,
+//           level,
+//           message,
+//         })}\n`,
+//       );
+//     };
 
-    if (!writeStream) {
-      fs.access(filepath, (e1) => {
-        /* istanbul ignore else */
-        if (e1) {
-          fs.mkdir(folder, { recursive: true }, (e2) => {
-            /* istanbul ignore else */
-            if (!e2) {
-              cb();
-            }
-          });
-        } else {
-          cb();
-        }
-      });
-    } else {
-      /* istanbul ignore next */
-      cb();
-    }
-  } catch (error) {}
-};
+//     if (!writeStream) {
+//       fs.access(filepath, (e1) => {
+//         /* istanbul ignore else */
+//         if (e1) {
+//           fs.mkdir(folder, { recursive: true }, (e2) => {
+//             /* istanbul ignore else */
+//             if (!e2) {
+//               cb();
+//             }
+//           });
+//         } else {
+//           cb();
+//         }
+//       });
+//     } else {
+//       /* istanbul ignore next */
+//       cb();
+//     }
+//   } catch (error) {}
+// };
 
 const createLogger = (level: string) => (service: string) => {
   const key = `${level.charAt(0).toLowerCase()}:${service}`;
@@ -79,7 +79,7 @@ const createLogger = (level: string) => (service: string) => {
     instance = logger.extend(key);
 
     // date + space + namespace + space
-    const sliceIndex = instance.namespace.length + 26;
+    // const sliceIndex = instance.namespace.length + 26;
 
     instance.log = (formatter: any, ...args: Array<any | (() => any)>) => {
       args.forEach((arg, i) => {
@@ -90,18 +90,18 @@ const createLogger = (level: string) => (service: string) => {
         }
       });
 
-      const message = util.format(formatter, ...args);
+      console.log(formatter, ...args);
 
-      process.stderr.write(`${message}\n`);
+      // process.stderr.write(`${message}\n`);
 
-      if (process.env.LOGGER_ROOT && process.env.DEBUG_COLORS === 'false') {
-        writeToFile(
-          message.slice(0, 24),
-          service,
-          level,
-          message.slice(sliceIndex),
-        );
-      }
+      // if (process.env.LOGGER_ROOT && process.env.DEBUG_COLORS === 'false') {
+      //   writeToFile(
+      //     message.slice(0, 24),
+      //     service,
+      //     level,
+      //     message.slice(sliceIndex),
+      //   );
+      // }
     };
 
     map.set(key, instance);
