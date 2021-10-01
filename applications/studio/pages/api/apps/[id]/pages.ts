b80869930707b915fb6df9842f8c8e2ed4850755
李文富 <liwenfu@crossjs.com>
2021-10-01 17:06:58 +0800

    fix(style): 修复创建页面接口

--- applications/studio/pages/api/apps/[id]/pages.ts
+++ applications/studio/pages/api/apps/[id]/pages.ts
@@ -1,10 +1,10 @@
-import { compose } from "@cofe/api";
-import { post } from "@cofe/io";
-import { withApiAuth } from "@/api/withApiAuth";
-import { withApiCatch } from "@/api/withApiCatch";
+import { compose } from '@cofe/api';
+import { post } from '@cofe/io';
+import { withApiAuth } from '@/api/withApiAuth';
+import { withApiCatch } from '@/api/withApiCatch';
 
 export default compose([withApiCatch(), withApiAuth()], async (req, res) => {
-  if (req.method === "POST") {
+  if (req.method === 'POST') {
     const page = await post(
       `${process.env.DB_URL}/api/apps/${req.query.id as string}/pages`,
       req.body,
