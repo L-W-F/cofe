b0d938170eab63f67a92e0611e89fce763c0f87b
李文富 <liwenfu@crossjs.com>
2021-10-01 17:02:16 +0800

    fix(style): 修复创建页面接口

--- applications/studio/pages/api/apps/[id]/pages.ts
+++ applications/studio/pages/api/apps/[id]/pages.ts
@@ -1,15 +1,19 @@
-import { compose } from '@cofe/api';
-import { post } from '@cofe/io';
-import { withApiAuth } from '@/api/withApiAuth';
-import { withApiCatch } from '@/api/withApiCatch';
+import { compose } from "@cofe/api";
+import { post } from "@cofe/io";
+import { withApiAuth } from "@/api/withApiAuth";
+import { withApiCatch } from "@/api/withApiCatch";
 
 export default compose([withApiCatch(), withApiAuth()], async (req, res) => {
-  if (req.method === 'POST') {
-    const page = await post(`${process.env.DB_URL}/api/apps/${req.query.id as string}/pages`, req.body, {
-      headers: {
-        Authorization: `Bearer ${req.cookies.token}`,
-      },
-    });
+  if (req.method === "POST") {
+    const page = await post(
+      `${process.env.DB_URL}/api/apps/${req.query.id as string}/pages`,
+      req.body,
+      {
+        headers: {
+          Authorization: `Bearer ${req.cookies.token}`,
+        },
+      }
+    );
 
     res.status(201).json(page);
   } else {
