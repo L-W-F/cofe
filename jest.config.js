e2ce629dd9505f41f50a3798aa2feef1d4e89ce3
crossjs <liwenfu@crossjs.com>
2021-11-21 01:50:10 +0800

    test(jest): 更新配置

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,6 @@ module.exports = {
   testEnvironment: 'jsdom',
   transform: { '\\.(j|t)sx?$': './jest.transformer.js' },
   transformIgnorePatterns: [
-    '/node_modules/(?!ansi-regex|array-iterate|bail|ccount|character-|cliui|colorette|comma-separated-tokens|flatted|hast-util-is-element|hast-util-to-html|hast-util-whitespace|html-void-elements|is-alphabetical|is-alphanumerical|is-decimal|is-hexadecimal|is-plain-obj|longest-streak|mdast-|micromark|micromark-|nanoid|parse-entities|property-information|space-separated-tokens|stringify-entities|trough|unified|unist-|vfile|vfile-message|y18n|yargs|yargs-parser|zwitch).+\\.js$',
+    '/node_modules/(?!ansi-regex|array-iterate|bail|ccount|character-|cliui|colorette|comma-separated-tokens|flatted|hast-util-is-element|hast-util-to-html|hast-util-whitespace|html-void-elements|is-alphabetical|is-alphanumerical|is-decimal|is-hexadecimal|is-plain-obj|lodash-|longest-streak|mdast-|micromark|micromark-|nanoid|parse-entities|property-information|space-separated-tokens|stringify-entities|trough|unified|unist-|vfile|vfile-message|y18n|yargs|yargs-parser|zwitch).+\\.js$',
   ],
 };
