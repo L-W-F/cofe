4e502cb4ff33a1c9407943ca83829d8387950152
李文富 <liwenfu@crossjs.com>
2022-01-04 10:40:01 +0800

    chore(gtm): add Google Tag Manager

---
+++ pages/_document.tsx
@@ -0,0 +1,25 @@
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends NextDocument {
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-5QT3RBV"
+              height="0"
+              width="0"
+              style="display:none;visibility:hidden"
+            />
+          </noscript>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
