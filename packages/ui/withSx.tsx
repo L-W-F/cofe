0937cafad1a8b9204cc4317bddce65a340b4db29
李文富 <liwenfu@crossjs.com>
2022-01-05 09:38:08 +0800

    refactor(ui): use __css in withSx

--- packages/ui/withSx.tsx
+++ packages/ui/withSx.tsx
@@ -1,11 +1,11 @@
 import React, { ComponentType, forwardRef } from 'react';
-import { CSSObject, useStyleConfig } from '@chakra-ui/react';
+import { useStyleConfig } from '@chakra-ui/react';
 
-export const withSx =
-  <T extends any = any>(name: string, defaultProps?: T) =>
-  (Component: ComponentType<T & { sx?: CSSObject }>) =>
-    forwardRef((props: T & { sx?: CSSObject }, ref) => {
-      const sx = useStyleConfig(name, props);
+export const withSx = <T extends any = any>(name: string, defaultProps?: Partial<T>) => (
+  Component: ComponentType<T>,
+) =>
+  forwardRef((props: T, ref) => {
+    const styles = useStyleConfig(name, props);
 
-      return <Component ref={ref} {...defaultProps} {...sx} {...props} />;
-    });
+    return <Component ref={ref} __css={styles} {...defaultProps} {...props} />;
+  });
